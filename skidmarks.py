7f693363130ba013e5daa3b7540bbac0c1dbcd38
liggettla <alexander.liggett@gmail.com>
2016-05-13 12:56:49 -0600

    added real data fetching

--- skidmarks.py
+++ skidmarks.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+
+#https://pypi.python.org/pypi/yahoo-finance
+#https://pypi.python.org/pypi/googlefinance
+
 from googlefinance import getQuotes
 from yahoo_finance import Share
 from yahoo_finance import Currency
@@ -14,3 +18,31 @@ else:
     makedirs('./userData')
 
 
+#Info
+securList =  ['AAPL', 'XLV']
+quotes = getQuotes(securList)
+for i in quotes:
+    index = i['Index']
+    symbol = i['StockSymbol']
+    price = float(i['LastTradePrice'])
+    time = i['LastTradeTime']
+    timeLong = i['LastTradeDateTimeLong']
+    dateTime = i['LastTradeDateTime']
+    lastCurrency = i['LastTradeWithCurrency']
+    ident = i['ID']
+
+
+from yahoo_finance import Share
+aapl = Share('AAPL')
+xlv = Share('XLV')
+
+#aapl = 96.25 02/22/16 290
+#xlv = 71.41 04/27/16 1400
+
+prevClose = float(xlv.get_prev_close())
+
+change = (price - prevClose) / prevClose * 100
+dayGain = change/100 * (1400 * prevClose)
+
+
+print symbol + '\t' + time + '\t' + str(price) + '\t' + str(change) + '\t$' + str(dayGain)
