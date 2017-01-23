3e743edf3d2cb0a7758dc4335d9211c6c6a2d21d
liggettla <alexander.liggett@gmail.com>
2017-01-23 13:43:04 -0700

    Updates

--- util/getQuote.py
+++ util/getQuote.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Make sure to first run:
 # source activate finance
 
@@ -5,9 +6,9 @@
 # and here: http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NYSE&render=download
 
 import pandas_datareader.data as web
-amzn = web.get_quote_yahoo('AMZN')
-print(amzn)
-print(type(amzn))
+qqq = web.get_quote_yahoo('QQQ')
+print(qqq)
+print(type(qqq))
 
 def getQuote(symbolList):
     quoteList = []
@@ -31,7 +32,7 @@ def getPrice():
     #from datetime import datetime
     import datetime
 
-    tickers = ['ORCL', 'TSLA', 'IBM', 'MSFT']
+    tickers = ['QQQ']
     start = datetime.datetime(2014,1,1)
     #end = datetime.datetime(2014,1,3)
     start = datetime.date.today() - datetime.timedelta (4)
@@ -44,7 +45,7 @@ def getPrice():
     adjCloseData = stockRawData.ix[sliceKey]
     print(adjCloseData)
 
-    ibmAdjCloseData = adjCloseData['IBM']
+    ibmAdjCloseData = adjCloseData['QQQ']
     print(ibmAdjCloseData)
 
 getPrice()
